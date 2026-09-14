962c0b48a73af86e47a55b6c7183a81de36ecb1c
Kevin Buchmann <k_buchmann@icloud.com>
2026-09-14 22:23:56 +0200

    Implement random color generation on page load and button click

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import './App.css';
-import logo from './logo.svg';
+import styled from '@emotion/styled';
+import randomColor from 'randomcolor';
+import { useState } from 'react';
 
+const StyledColorDiv = styled.div`
+  background-color: ${(props) => props.backgroundColor};
+  padding: 20px;
+`;
 export default function App() {
+  const [randomHexcolor, setRandomHexcolor] = useState(randomColor());
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button
+        onClick={() => {
+          setRandomHexcolor(randomColor());
+        }}
+      >
+        Generate
+      </button>
+      <StyledColorDiv backgroundColor={randomHexcolor}>
+        Generated Color: {randomHexcolor}
+      </StyledColorDiv>
+    </>
   );
 }
