b940512354fb1bfe25550a3d0c7c7a53d3062766
Kevin Buchmann <k_buchmann@icloud.com>
2026-09-15 16:46:41 +0200

    Style 'Generate' button

--- src/App.js
+++ src/App.js
@@ -6,12 +6,24 @@ import { useState } from 'react';
 const StyledColorDiv = styled.div`
   background-color: ${(props) => props.backgroundColor};
   padding: 20px;
+  text-align: center;
+  font-size: 20px;
 `;
 export default function App() {
   const [randomHexcolor, setRandomHexcolor] = useState(randomColor());
   return (
     <>
       <button
+        style={{
+          padding: '10px 10px 20px',
+          display: 'block',
+          width: '200px',
+          margin: '16px auto',
+          backgroundColor: 'black',
+          color: 'white',
+          fontWeight: 'bold',
+          fontSize: '30px',
+        }}
         onClick={() => {
           setRandomHexcolor(randomColor());
         }}
